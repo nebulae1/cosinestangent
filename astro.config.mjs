0a5fc9fd86af75981bd607eddc5d6cfbb71dfae1
Craig Helton <nebulae1@users.noreply.github.com>
2024-03-06 05:48:06 -0500

    updates for astro vercel package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
-
+// Import /static for a static site
+import vercelStatic from '@astrojs/vercel/static';
 import icon from "astro-icon";
 import sitemap from "@astrojs/sitemap";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://cosinestangent.com',
-  integrations: [
-    icon(),
-    sitemap()],
+  integrations: [icon(), sitemap()],
+  output: 'static',
+  adapter: vercelStatic(),
 });
